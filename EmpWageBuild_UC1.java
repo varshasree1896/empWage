bcc00b21cf311dd35c0f2629c783fc829e13cc42
varshasree1896 <91524862+varshasree1896@users.noreply.github.com>
2022-01-16 17:43:16 +0530

    Add files via upload

---
+++ EmpWageBuild_UC1.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class EmpWageBuild_UC1 {
+    public static void main(String[] args) {
+        // Constants
+        int IS_FULL_TIME = 1;
+        //Computation
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_FULL_TIME)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+
+        }
+    }
+
